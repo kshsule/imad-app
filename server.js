5d17505e8626820b485c9b348ea42440f01bbdb0
kshsule <kshitijsule24@gmail.com>
2018-02-26 20:43:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/article-one', function(req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
-app.get('/article-two', function(req,res) {
-    res.send("Article two gets served");
+app.get('/article-two', function(req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
