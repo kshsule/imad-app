c96b304fda5c2ca7b76a4cbb67e99a1f1f212c36
kshsule <kshitijsule24@gmail.com>
2018-03-02 23:38:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = "New Text";
+
+var img = document.getElementById("dragon");
+img.onclick = function(){
+    img.style.marginLeft = '150px';
+};
