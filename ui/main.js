ec079783011ca55bff3c5252031acddc51fe38bf
kshsule <kshitijsule24@gmail.com>
2018-03-02 23:50:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ var element = document.getElementById('main-text');
 element.innerHTML = "New Text";
 
 var img = document.getElementById("dragon");
-var moveLEft = 0;
+var moveLeft = 0;
 function moveRight(){
     moveLeft = moveLeft + 1;
     img.style.marginLeft = moveLeft;
