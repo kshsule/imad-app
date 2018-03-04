1991ef3e988ef6dd421f0678b3e450d3895a79eb
kshsule <kshitijsule24@gmail.com>
2018-03-04 17:02:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,12 +3,4 @@ var element = document.getElementById('main-text');
 element.innerHTML = "New Text";
 
 var img = document.getElementById("dragon");
-var moveLeft = 0;
-function moveRight(){
-    moveLeft = moveLeft + 5;
-    img.style.marginLeft = moveLeft + 'px';
-}
-img.onclick = function(){
-    var interval = setInterval(moveRight,50);
-    
-};
+
